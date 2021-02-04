949bbe76090fef99dbdd3fc134a3a3bd34b4ef88
Keith Nolan <g00351932@gmit.ie>
2021-02-04 16:28:03 +0000

    Bit shifting operations

--- bit-operations.c
+++ bit-operations.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+void bin_print(int i){
+    // number of bits in an integer
+    int j = sizeof(int) * 8;
+    int k;
+
+    for(j-- ; j >=0; j--){
+
+        k = ((10 << j)& i) ? 1 : 0;
+        printf("%d", k );
+
+
+    }
+}
+
+int main(int argc, char *argv[])
 {
-    printf("%s\n", "Hello, world!!!");
+    int i =241;
+    printf("Original:  ");
+    bin_print(i);
+    printf("\n");
+
+    for(int j = 0; j < 40; j++){
+        printf("%3d << %2d: ", i, j);
+        bin_print(i << j);
+        printf("\n");
+    }
 
     return 0;
 }
